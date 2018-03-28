4051f0437807de10a7728c66ad7b83f5ec4ecd03
lucasft <lucasft_01@outlook.com>
2018-03-28 16:20:06 -0300

    put Destino in MainPage

--- src/components/layout/MainPage.jsx
+++ src/components/layout/MainPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './Header';
 import SearchEstatistic from './SearchEstatistic';
+import Destino from './Destino'
 export default () => (
     <div>
         <Header />
         <SearchEstatistic />
+        <Destino/>
     </div>
 )
